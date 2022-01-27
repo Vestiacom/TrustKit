c1c3fa86b5d3fb2bf77448fbadc06661ac3269f4
Hubert Pietrusiak <hubert.pietrusiak@gmail.com>
2022-01-27 10:45:29 +0100

    Update parse_configuration.m

--- TrustKit/parse_configuration.m
+++ TrustKit/parse_configuration.m
@@ -68,12 +68,12 @@ NSDictionary *parseTrustKitConfiguration(NSDictionary *trustKitArguments)
     
     for (NSString *domainName in trustKitArguments[kTSKPinnedDomains])
     {
-        // Sanity checks on the domain name
-        if (GetRegistryLength([domainName UTF8String]) == 0)
-        {
-            [NSException raise:@"TrustKit configuration invalid"
-                        format:@"TrustKit was initialized with an invalid domain %@", domainName];
-        }
+//         // Sanity checks on the domain name
+//         if (GetRegistryLength([domainName UTF8String]) == 0)
+//         {
+//             [NSException raise:@"TrustKit configuration invalid"
+//                         format:@"TrustKit was initialized with an invalid domain %@", domainName];
+//         }
         
         
         // Retrieve the supplied arguments for this domain
